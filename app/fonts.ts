56819a26343110abb89416a5b8175414a92f2dd6
yuuthod <yuuthod@gmail.com>
2024-01-12 13:41:59 +0900

    update fonts & font file

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local';
 export const roboto = localFont({
   src: [
     {
-      path: './fonts/NotoSansKR-Thin.ttf',
+      path: '../public/fonts/NotoSansKR-Thin.ttf',
       weight: '100',
       style: 'Thin'
     },
     {
-      path: './fonts/NotoSansKR-ExtraLight.ttf',
+      path: '../public/fonts/NotoSansKR-ExtraLight.ttf',
       weight: '200',
       style: 'extraLight'
     },
     {
-      path: './fonts/NotoSansKR-Light.ttf',
+      path: '../public/fonts/NotoSansKR-Light.ttf',
       weight: '300',
       style: 'light'
     },
     {
-      path: './fonts/NotoSansKR-Regular.ttf',
+      path: '../public/fonts/NotoSansKR-Regular.ttf',
       weight: '400',
       style: 'regular'
     },
     {
-      path: './fonts/NotoSansKR-Medium.ttf',
+      path: '../public/fonts/NotoSansKR-Medium.ttf',
       weight: '500',
       style: 'medium'
     },
     {
-      path: './fonts/NotoSansKR-semiBold.ttf',
+      path: '../public/fonts/NotoSansKR-semiBold.ttf',
       weight: '600',
       style: 'semiBold'
     },
     {
-      path: './fonts/NotoSansKR-Bold.ttf',
+      path: '../public/fonts/NotoSansKR-Bold.ttf',
       weight: '700',
       style: 'bold'
     },
     {
-      path: './fonts/NotoSansKR-ExtraBold.ttf',
+      path: '../public/fonts/NotoSansKR-ExtraBold.ttf',
       weight: '800',
       style: 'extraBold'
     },
     {
-      path: './fonts/NotoSansKR-Black.ttf',
+      path: '../public/fonts/NotoSansKR-Black.ttf',
       weight: '900',
       style: 'black'
     }
