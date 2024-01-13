7c7430062fa5c08fcf032d465481a67f60bd048c
yuuthod <yuuthod@gmail.com>
2024-01-13 21:23:14 +0900

    update style & next.config
    - global style 추가
    - 반응형 작업을 위한 mixin.scss 추가
    - svg 파일 사용을 위한 svgr/webpack lib 추가
    - classnames lib 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     imageSizes: [16, 32, 48, 64],
     deviceSizes: [640, 750, 828, 1080]
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
   }
 };
 
