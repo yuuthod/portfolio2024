f2fce52f1dd38e551d364dfda742d378b5708022
yuuthod <yuuthod@gmail.com>
2024-01-18 01:09:10 +0900

    update fonts.ts & font file
    - vercel 배포 시 font 파일의 경로를 인식하지 못하는 문제를 해결하기 위해 font 파일명을 소문자로 변경

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local';
 export const roboto = localFont({
   src: [
     {
-      path: '../public/fonts/NotoSansKR-Thin.ttf',
+      path: '../public/fonts/notosans_kr-thin.ttf',
       weight: '100',
       style: 'Thin'
     },
     {
-      path: '../public/fonts/NotoSansKR-ExtraLight.ttf',
+      path: '../public/fonts/notosans_kr-extralight.ttf',
       weight: '200',
       style: 'extraLight'
     },
     {
-      path: '../public/fonts/NotoSansKR-Light.ttf',
+      path: '../public/fonts/notosans_kr-light.ttf',
       weight: '300',
       style: 'light'
     },
     {
-      path: '../public/fonts/NotoSansKR-Regular.ttf',
+      path: '../public/fonts/notosans_kr-regular.ttf',
       weight: '400',
       style: 'regular'
     },
     {
-      path: '../public/fonts/NotoSansKR-Medium.ttf',
+      path: '../public/fonts/notosans_kr-medium.ttf',
       weight: '500',
       style: 'medium'
     },
     {
-      path: '../public/fonts/NotoSansKR-semiBold.ttf',
+      path: '../public/fonts/notosans_kr-semibold.ttf',
       weight: '600',
       style: 'semiBold'
     },
     {
-      path: '../public/fonts/NotoSansKR-Bold.ttf',
+      path: '../public/fonts/notosans_kr-bold.ttf',
       weight: '700',
       style: 'bold'
     },
     {
-      path: '../public/fonts/NotoSansKR-ExtraBold.ttf',
+      path: '../public/fonts/notosans_kr-extrabold.ttf',
       weight: '800',
       style: 'extraBold'
     },
     {
-      path: '../public/fonts/NotoSansKR-Black.ttf',
+      path: '../public/fonts/notosans_kr-black.ttf',
       weight: '900',
       style: 'black'
     }
