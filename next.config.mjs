c0e05c14497488e55ea1fa5e17fd77d76cc0c2cc
yuuthod <yuuthod@gmail.com>
2024-02-14 14:29:14 +0900

    update 컬러 및 레이아웃
    - 메인 레이아웃 변경
    - color.scss 변경
    - next.config image url 변경
    - 사이드바 디자인 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['yurim-private.s3.ap-northeast-2.amazonaws.com'],
+    domains: ['static.ruccess.site'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
